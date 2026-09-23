96adffa477edef06fa1786998fce828586e0a71d
Duy Nguyen <duy.nguyen@giftee.co>
2026-09-23 15:53:54 +0700

    AI: Contact form (#12)
    Co-authored-by: Agentouch Bot <agent@agentouch.local>

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticatedSessionController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,6 @@ Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('lo
 Route::post('login', [AuthenticatedSessionController::class, 'store'])
     ->middleware('throttle:5,1')
     ->name('login.store');
+
+Route::get('contacts', [ContactController::class, 'create'])->name('contacts');
+Route::post('contacts', [ContactController::class, 'store'])->name('contacts.store');
